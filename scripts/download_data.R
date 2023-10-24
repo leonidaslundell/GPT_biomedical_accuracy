aca392e209bf844e390e2dbb236ddaad7f1c91ee
leonidaslundell <leonidas.lundell@gmail.com>
2023-10-24 22:46:01 +0200

    Major improvements, identified new issues with identifying correct

--- scripts/download_data.R
+++ scripts/download_data.R
@@ -1,98 +1,167 @@
 wd <- Sys.getenv("wd")
+data <- paste0(wd, "/data/")
 api_key <- Sys.getenv("api_key")
 
 source(paste0(wd, "/src/functions.R"))
-library(R.utils)
+library(data.table)
+library(progress)
 
-# Genes to download
+####################################################
+#Downloading data from a textmining resource
+# from https://diseases.jensenlab.org/Downloads
+# https://www.sciencedirect.com/science/article/pii/S1046202314003831
 
-library(org.Hs.eg.db)
-library(reactome.db)
+#using it in the broadest sense without any prefiltering in confidence
 
-reactome_genes <- reactomeEXTID2PATHID |> as.list()
-human <- sapply(reactome_genes, \(x){
-  grepl('HSA', x) |> any()  
-})
-entrez <- reactome_genes[human] |> names() |> unique()
-human_genes <- select(org.Hs.eg.db, keys=entrez, columns=c("SYMBOL"), keytype="ENTREZID")
+diseases <- fread(paste0(data, "human_disease_textmining_filtered.tsv"))[,1:4]
+colnames(diseases) <- c("identifier", "symbol", "DO_ID", "disease")
+#drop noncoding
+diseases <- diseases[!grepl("rRNA|hsa_circ|sno|hsa|AS|orf|ENS|\\.|RNA", symbol)]
+#Obsolete DOID as as as Type 2 diabebets labeled
+diseases[disease == "DOID:0081062", "disease"] <- "Type 2 diabetes mellitus"
 
-# Query chatGPT and evaluate results
-# While you should do only one thing in functions, I wanted 100 genes, and the preprocessing tells us whether
-# the gene query was sucessfull.
+####################################################
+#donwload data from chatGPT
+n_genes <- 100
 
-GPT_response <- NULL
-
-while(length(GPT_response)<1000){
-  
-  #some APIs seem to crash sometimes. Restart after 1 muntes.
-  withTimeout({
-    symbol <- sample(na.omit(human_genes$SYMBOL), 1)
-    print(symbol)
-    
-    disease <- gene_to_uniprot_disease(symbol) 
-    
-    #some genes have no diseases connected to them.
-    if(!is.null(disease)){
-      GPT_response[[symbol]] <- NULL
-      GPT_response[[symbol]]$disease <- disease
-      
-      #to not pass the per minute query limits
-      gpt <- try(GPT_query(symbol, api_key))
-      if(class(gpt) == "try-error"){
-        #Sometimes limit is reached in any case. 
-        gpt <- GPT_query(symbol, api_key)
-      }
-      
-      #check if the response is as requested in terms of structure
-      length_diseases <- sapply(gpt$disease, \(xx){
-          stringr::str_count(xx, '\\w+')<14
-        })
-    
-      if(any(length_diseases)){
-        
-        gpt$disease <- gpt$disease[length_diseases]
-        accuracy <- sapply(gpt$disease, GO = disease, mean_calc = T, word_by_word_lcs)
-        
-        gpt_refernce_predictions <- try(sapply(gpt$reference, pubmed_query))
-        
-        if(class(gpt_refernce_predictions) == "try-error"){
-          gpt_refernce_predictions <- "pubmed query failed"
-        }
-        
-        gene_popularity <- try(pubmed_query(gene = symbol))
-        if(class(gene_popularity) == "try-error"){
-          gene_popularity <- "pubmed query failed"
-        }
-        
-        GPT_response[[symbol]]$accuracy <- accuracy
-        GPT_response[[symbol]]$gene_references <- gene_popularity
-        GPT_response[[symbol]]$gpt_out <- gpt
-        GPT_response[[symbol]]$gpt_refernce_predictions <- gpt_refernce_predictions
-      }else{
-        print(gpt$disease[!length_diseases])
-        GPT_response[[symbol]] <- NULL
-      }
-      
-    }else{
-      print("No gene context")
-    }
-    
-  }, timeout = 60, onTimeout = "silent")
+#The openai api calls stall for some weird reason. Manual work around using the function below.
+#stupid function to restart the loops...
+remaining <- function(x){
+  sapply(x, \(x) class(x) == 'try-error' | is.null(x)) |> which() |> names()
 }
 
-# Cleaning up the data
+#############
+#well known genes (top 5%)
 
-# sometimes the response has padding in the form of GENE plays a role \n \n. Below cleans that up.
-plays_a_role <- sapply(GPT_response, \(x){
-  nchar(x$gpt_out$disease)[1]>60 & nchar(x$gpt_out$disease)[2]==0
-})
+well_known_subset <- generate_lists(c(0,0.05), diseases)
+well_known_subset$genes <- sample(well_known_subset$genes, n_genes, replace = F)
 
-for(gene in which(plays_a_role)){
-  GPT_response[[gene]]$gpt_out$disease <- GPT_response[[gene]]$gpt_out$disease[-1:-2]
-  GPT_response[[gene]]$accuracy <- GPT_response[[gene]]$accuracy[-1:-2]
+well_known_genes_3.5 <- well_known_genes_4 <- well_known_freeform <- vector(mode = "list", length = n_genes)
+names(well_known_genes_3.5) <- names(well_known_genes_4) <- names(well_known_freeform) <- well_known_subset$genes
+
+pb <- progress_bar$new(total = 100)
+for(gene in remaining(well_known_genes_3.5)){
+  pb$tick()
+  Sys.sleep(2)
+  well_known_genes_3.5[[gene]] <- try(GPT_query_list(gene, disease_list = well_known_subset$disease, api_key = api_key, model = "gpt-3.5-turbo"))
 }
 
-#droping any cases not picked up by the above filters.
-GPT_response <- GPT_response[sapply(GPT_response, length)>1]
+pb <- progress_bar$new(total = 100)
+for(gene in remaining(well_known_genes_4)){
+  pb$tick()
+  Sys.sleep(2)
+  well_known_genes_4[[gene]] <- try(GPT_query_list(gene, disease_list = well_known_subset$disease, api_key = api_key, model = "gpt-4"))
+}
 
-save(GPT_response, file = paste0(wd, '/data/GPT_response_improved_query.Rdata'))
+pb <- progress_bar$new(total = 100)
+for(gene in remaining(well_known_freeform)){
+  pb$tick()
+  Sys.sleep(2)
+  well_known_freeform[[gene]] <- try(GPT_query_freeform(gene, api_key = api_key, model = "gpt-4"))
+}
+
+#############
+#intermediate known genes, 20%-25%
+
+intermediate_known_subset <- generate_lists(c(.20, 0.25), diseases)
+remove <- intersect(intermediate_known_subset$genes, well_known_subset$genes)
+intermediate_known_subset$genes <- intermediate_known_subset$genes[!intermediate_known_subset$genes %in% remove]
+intermediate_known_subset$genes <- sample(intermediate_known_subset$genes, n_genes, replace = F)
+
+intermediate_known_genes_3.5 <- intermediate_known_genes_4 <- vector(mode = "list", length = n_genes)
+names(intermediate_known_genes_3.5) <- names(intermediate_known_genes_4) <- intermediate_known_subset$genes
+
+pb <- progress_bar$new(total = 100)
+for(gene in remaining(intermediate_known_genes_3.5)){
+  pb$tick()
+  Sys.sleep(2)
+  intermediate_known_genes_3.5[[gene]] <- try(GPT_query_list(gene, disease_list = intermediate_known_subset$disease, api_key = api_key, model = "gpt-3.5-turbo"))
+}
+
+pb <- progress_bar$new(total = 100)
+for(gene in remaining(intermediate_known_genes_4)){
+  pb$tick()
+  Sys.sleep(2)
+  intermediate_known_genes_4[[gene]] <- try(GPT_query_list(gene, disease_list = intermediate_known_subset$disease, api_key = api_key, model = "gpt-4"))
+}
+
+# save(list = c("well_known_genes_3.5", "well_known_genes_4", "intermediate_known_genes_3.5", "intermediate_known_genes_4", "well_known_freeform"),
+#      file = paste0(data, "GPT_raw_response.Rdata"))
+
+####################################################
+#Manually review all responses to format them so that they are comparable
+
+intermediate_known_genes_3.5 <- manual_review(intermediate_known_genes_3.5)
+intermediate_known_genes_4 <- manual_review(intermediate_known_genes_4)
+well_known_genes_3.5 <- manual_review(well_known_genes_3.5)
+well_known_genes_4 <- manual_review(well_known_genes_4)#when several diseases predicted pick last
+
+# save(list = c("well_known_genes_3.5", "well_known_genes_4", "intermediate_known_genes_3.5", "intermediate_known_genes_4"),
+#      file = paste0(data, "GPT_manual_review_response.Rdata"))
+
+####################################################
+#evaluate response
+load(paste0(data, "GPT_manual_review_response.Rdata"))
+
+pb <- progress_bar$new(total = 100)
+for(gene in names(intermediate_known_genes_3.5)){
+  pb$tick()
+  intermediate_known_genes_3.5[[gene]] <- evaluate_responses(intermediate_known_genes_3.5[[gene]], gene)
+}
+
+pb <- progress_bar$new(total = 100)
+for(gene in names(intermediate_known_genes_4)){
+  pb$tick()
+  intermediate_known_genes_4[[gene]] <- evaluate_responses(intermediate_known_genes_4[[gene]], gene)
+}
+
+pb <- progress_bar$new(total = 100)
+for(gene in names(well_known_genes_3.5)){
+  pb$tick()
+  well_known_genes_3.5[[gene]] <- evaluate_responses(well_known_genes_3.5[[gene]], gene)
+}
+
+pb <- progress_bar$new(total = 100)
+for(gene in names(well_known_genes_4)){
+  pb$tick()
+  well_known_genes_4[[gene]] <- evaluate_responses(well_known_genes_4[[gene]], gene)
+}
+
+well_known_freeform <- evaluate_freeform(well_known_freeform)
+
+#############
+#comments on how I associated. This is not everything...
+
+# T2D == metabolic syndrome
+# Alzehimer == dementia
+# Congenital Secretory Sodium Diarrhea
+# Major Depressive Disorder == Mood disorder
+# Tuberous sclerosis == Tuberous Sclerosis Complex
+# Rheumatic disease == Rheumatic arthitis
+# Epilepsy == Action myoclonus-renal failure syndrome
+# Hemolytic anemia == anemia
+
+#intersting how close: Arginase deficiency, Argininosuccinic aciduria
+# intersting variation... Diabetes Mellitus Type 2
+
+# save(list = c("well_known_genes_3.5", "well_known_genes_4", "intermediate_known_genes_3.5", "intermediate_known_genes_4", "well_known_freeform"),
+#      file = paste0(data, "GPT_response_full.Rdata"))
+
+####################################################
+# Reformat into tidy
+#load(paste0(data, "GPT_response_full.Rdata"))
+
+#typo in T/F
+well_known_freeform$CSF3$accuracy <- well_known_freeform$IL18$accuracy <- T
+
+well_known <- reformat_results(well_known_genes_3.5, well_known_genes_4)
+intermediate_known <- reformat_results(intermediate_known_genes_3.5, intermediate_known_genes_4)
+#note that there is a major warning here, rownames have been deduplicated by adding 1 at the end...
+# all(stringr::str_sub(rownames(well_known)[101:200], end = -2) == rownames(well_known)[1:100])
+
+sapply(well_known_freeform, \(x) as.logical(x$accuracy)) |> table()
+
+well_known_freeform <- sapply(well_known_freeform, \(x) as.logical(x$accuracy))
+
+# save(list = c("well_known","intermediate_known","well_known_freeform"), 
+#      file = paste0(data, "GPT_response.Rdata"))
--- src/functions.R
+++ src/functions.R
@@ -1,6 +1,7 @@
 library(rjson)
 library(openai)
-library(svMisc)
+# library(svMisc)
+library(R.utils)
 
 pubmed_query <- function(title = "Time-restricted feeding alters lipid and amino acid metabolite rhythmicity without perturbing clock gene expression",
                          gene = NULL){
@@ -13,8 +14,16 @@ pubmed_query <- function(title = "Time-restricted feeding alters lipid and amino
   }
   
   pubmed_api_query <- gsub(" ", "%20", pubmed_api_query)
-  reference <- fromJSON(file=pubmed_api_query, simplify=FALSE)
-  reference <- as.numeric(reference$esearchresult$count)
+  reference <- withTimeout({
+    try(fromJSON(file=pubmed_api_query, simplify=FALSE))
+    }, timeout = 60, onTimeout = "silent")
+  
+  if(class(reference) == "try-error"){
+    reference <- NA
+  }else{
+    reference <- as.numeric(reference$esearchresult$count) 
+  }
+  
   return(reference)
 }
 
@@ -35,90 +44,168 @@ reformat <- function(x){
   return(x)
 }
 
-GPT_query <- function(symbol = "GLUT4", api_key){
+GPT_query_list <- function(symbol = "GLUT4", disease_list, api_key, model = "gpt-3.5-turbo", full = F){
   
-  prompt_prime <- "Answer as an expert medical doctor, molecular biologist, and geneticist."
-  prompt_disease <- "Which clinically relevant conditions, specific diseases, or syndromes does the gene GENESYMBOL play a role in."
-  prompt_disease_followup <- "From the text below, give me only the conditions, specific diseases, or syndromes. Format answer only with a list of specific names separated by new line without any elaboration. Text:\n"
-  
-  prompt_reference <- "For the text below, give me up to 5 scientific literature references to further my understanding about GENESYMBOL. Answer only with article title. Do not elaborate. Do not include authors. Separate by new line. Text:\n"
-  
-  prompt_disease <- gsub('GENESYMBOL', symbol, prompt_disease)
-  prompt_reference <- gsub('GENESYMBOL', symbol, prompt_reference)
-  
-  gpt_answer_disease <- create_chat_completion(openai_api_key = api_key,
-                                               model = "gpt-3.5-turbo",
-                                               temperature = 0.5,
-                                               messages = list(list("role" = "system",
-                                                                    "content" = prompt_prime),
-                                                               list("role" = "user",
-                                                                    "content" = prompt_disease)
-                                               )
-  )
-  
-  prompt_disease_followup_gene <- paste0(prompt_disease_followup, gpt_answer_disease$choices$message.content)
-  gpt_answer_disease_followup <- create_chat_completion(openai_api_key = api_key,
-                                                        model = "gpt-3.5-turbo",
-                                                        temperature = 0.5,
-                                                        messages = list(list("role" = "system",
-                                                                             "content" = prompt_prime),
-                                                                        list("role" = "user",
-                                                                             "content" = prompt_disease_followup_gene)
-                                                        )
-  )
-  
-  prompt_reference_gene <- paste0(prompt_reference, gpt_answer_disease$choices$message.content)
-  gpt_answer_reference <- create_chat_completion(openai_api_key = api_key,
-                                                 model = "gpt-3.5-turbo",
-                                                 temperature = 0.5,
+  #adding a time-out because it seems to crash some times
+  withTimeout({
+    prompt_prime <- "Answer as an expert medical doctor, molecular biologist, and geneticist."
+    
+    prompt_disease <- "Which disease from the diseaselist below is gene GENESYMBOL involved in? Reply only with the name of the disease. Diseaselist:"
+    prompt_reference <- "Give me 5 or fewer scientific literature references to further my understanding of gene GENESYMBOL. Answer only with article title. Do not elaborate. Do not include authors. Separate by new line. Text:\n"
+    
+    prompt_disease <- gsub("GENESYMBOL", symbol, prompt_disease)
+    prompt_reference <- gsub("GENESYMBOL", symbol, prompt_reference)
+    
+    prompt_gene_disease <- paste0(prompt_disease, paste0(" ", disease_list, collapse = ", "))
+    
+    # print(prompt_gene_disease)
+    
+    gpt_answer_disease <- create_chat_completion(openai_api_key = api_key,
+                                                 model = model,
+                                                 # temperature = 0.5,
                                                  messages = list(list("role" = "system",
                                                                       "content" = prompt_prime),
                                                                  list("role" = "user",
-                                                                      "content" = prompt_reference_gene)
+                                                                      "content" = prompt_gene_disease)
                                                  )
-  )
-  gpt_answer_disease_list <- reformat(gpt_answer_disease_followup$choices$message.content)
-  gpt_answer_reference <- reformat(gpt_answer_reference$choices$message.content)
-  
-  gpt_answer <- list(reference = gpt_answer_reference,
-                     disease = gpt_answer_disease_list,
-                     full_response = gpt_answer_disease$choices$message.content)
-  
-  return(gpt_answer)
+    )
+    
+    gpt_answer_reference <- create_chat_completion(openai_api_key = api_key,
+                                                   model = "gpt-3.5-turbo",
+                                                   # temperature = 0.5,
+                                                   messages = list(list("role" = "system",
+                                                                        "content" = prompt_prime),
+                                                                   list("role" = "user",
+                                                                        "content" = prompt_reference)
+                                                   )
+    )
+    gpt_answer_disease_reformat <- gsub(".*: |\\.","", gpt_answer_disease$choices$message.content)
+    gpt_answer_reference <- reformat(gpt_answer_reference$choices$message.content)
+    
+    if(full){
+      gpt_answer <- list(reference = gpt_answer_reference,
+                         disease = gpt_answer_disease_reformat,
+                         full_response = gpt_answer_disease)
+    }else{
+      gpt_answer <- list(reference = gpt_answer_reference,
+                         disease = gpt_answer_disease_reformat) 
+    }
+    
+    return(gpt_answer)
+  }, timeout = 60, onTimeout = "silent")
 }
 
-gene_to_uniprot_disease <- function(symbol = "GLUT4"){
-  symbol <- toupper(symbol)
-  query <- paste0(
-    "https://rest.uniprot.org/uniprotkb/search?query=organism_id:9606+AND+", 
-    symbol,
-    "&format=json"
-  )
-  uniprot <- fromJSON(file=query, simplify=FALSE)
+GPT_query_freeform <- function(symbol = "GLUT4", disease_list, api_key, model = "gpt-3.5-turbo", full = F){
   
-  disease <- sapply(uniprot[["results"]], \(res){
-    sapply(res$comments, \(comment){
-      c(comment$disease$diseaseId, comment$disease$acronym)
-    })
-  }) |> unlist(use.names = FALSE)
-  
-  return(disease)
+  #adding a time-out because it seems to crash some times
+  withTimeout({
+    prompt_prime <- "Answer as an expert medical doctor, molecular biologist, and geneticist."
+    prompt_gene_disease <- 'Which relevant conditions, specific diseases, or syndromes does the gene GENESYMBOL play a role in. Format answer only with a list of specific names separated by new line without any elaboration. Limit your answer only 1 disease, the most common.'
+    prompt_gene_disease <- gsub("GENESYMBOL", symbol, prompt_gene_disease)
+    
+    gpt_answer_disease <- create_chat_completion(openai_api_key = api_key,
+                                                 model = model,
+                                                 # temperature = 0.5,
+                                                 messages = list(list("role" = "system",
+                                                                      "content" = prompt_prime),
+                                                                 list("role" = "user",
+                                                                      "content" = prompt_gene_disease))
+    )
+    gpt_answer <- gpt_answer_disease$choices$message.content
+    
+    return(gpt_answer)
+  }, timeout = 60, onTimeout = "silent")
 }
 
-word_by_word_lcs <- function(GPT, GO, mean_calc = T){
-  GPT_word <- strsplit(tolower(GPT), "\\s+")[[1]]
+generate_lists <- function(interval, disease_table){
+  disease_counts <- disease_table$disease |> table() |> sort(decreasing = T)
   
-  GO <- paste0(gsub(',', '', tolower(GO)), collapse = ' ')
+  disease_interval <- quantile(1:length(disease_counts), na.rm = T, probs = c(interval[1],interval[2]))
+  disease_interval <- disease_interval[1]:disease_interval[2] |> ceiling()
+  disease_subset <- names(disease_counts)[disease_interval]
   
-  GPT_GO_sim <- sapply(GPT_word, \(x){
-    GPT_similarity <- CEGO::distanceStringLCStr(x, GO)
-    GPT_similarity <- nchar(GO) - GPT_similarity
-    GPT_similarity <- GPT_similarity/nchar(x)
-    GPT_similarity
-  })
-  if(mean_calc){
-    GPT_GO_sim <- mean(GPT_GO_sim)
+  gene_counts <- disease_table[disease %in% disease_subset, symbol] |> table() |> sort(decreasing = T)
+  gene_interval <- quantile(1:length(gene_counts), na.rm = T, probs = c(interval[1],interval[2]))
+  gene_interval <- gene_interval[1]:gene_interval[2] |> ceiling()
+  gene_subset <- names(gene_counts)[disease_interval]
+  
+  subset <- list(
+    disease = disease_subset,
+    genes = gene_subset
+  )
+  
+  return(subset)
+}
+
+manual_review <- function(GTP_response){
+  for(i in names(GTP_response)){
+    response <- GTP_response[[i]]$disease
+    cat(response)
+    disease <- readline()
+    
+    if(nchar(disease)==1 & !is.na(suppressWarnings(as.numeric("1")))){
+      disease <- as.numeric(disease)
+      diseases_words <- strsplit(GTP_response[[i]]$disease, " ")[[1]]
+      start <- length(diseases_words) - (disease - 1)
+      end <- length(diseases_words)
+      
+      GTP_response[[i]]$disease <- paste0(diseases_words[start:end], collapse = " ")
+      } else if (nchar(disease)>0){
+      GTP_response[[i]]$disease <- disease
+    }
+    
+    cat(paste0(which(i == names(GTP_response)), " ------------\n"))
   }
   
-  return(GPT_GO_sim)
+  return(GTP_response)
+}
+
+evaluate_responses <- function(GPT_response, gene){
+  
+  GPT_response$gene_hits <- pubmed_query(gene = gene)
+  GPT_response$disease_correct <- any(GPT_response$disease %in% diseases[symbol == gene, disease])
+  GPT_response$reference_correct <- sapply(GPT_response$reference, pubmed_query, USE.NAMES = F)
+  
+  return(GPT_response)
+}
+
+evaluate_freeform <- function(GPT_response){
+  
+  for(gene in names(GPT_response)){
+    print("\n")
+    print(diseases[symbol == gene, sort(unique(disease))])
+    print("--------------")
+    print(GPT_response[[gene]])
+    i <- which(names(GPT_response) == gene)
+    
+    disease <- GPT_response[[gene]]
+    GPT_response[[gene]] <- NULL#reformating for nested list
+    GPT_response[[gene]]$disease <- disease
+    
+    print(paste0(i, "--------------"))
+    response_accuracy <- readline("True/False\n")
+    GPT_response[[gene]]$accuracy <- response_accuracy
+    }
+  
+  
+  return(GPT_response)
+}
+
+reformat_results <- function(res_3.5, res_4){
+  res_3.5 <- sapply(res_3.5, \(x){
+    c(correct = x$disease_correct, 
+      gene_hits = x$gene_hits, 
+      ref_correct = sum(x$reference_correct>0)/length(x$reference_correct),
+      model = "GPT-3.5")
+  }) |> t() |> data.frame() 
+  res_4 <- sapply(res_4, \(x){
+    c(correct = x$disease_correct, 
+      gene_hits = x$gene_hits, 
+      ref_correct = sum(x$reference_correct>0)/length(x$reference_correct),
+      model = "GPT-4")
+  }) |> t() |> data.frame() 
+  res <- rbind(
+    res_3.5, res_4
+  )
+  return(res)
 }
